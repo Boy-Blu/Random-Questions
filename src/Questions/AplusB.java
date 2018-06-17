f8b1a0fa5ac2a930be7dfbad1684279387e03548
Anthony <35087098+agrusk@users.noreply.github.com>
2018-06-16 21:07:36 -0400

    AplusB added

---
+++ src/Questions/AplusB.java
@@ -0,0 +1,19 @@
+package Questions;
+
+import java.util.*;
+
+public class AplusB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner in = new Scanner(System.in);
+
+	        int N = in.nextInt();
+	        for (int i = 0; i < N; i++) {
+	            int a = in.nextInt();
+	            int b = in.nextInt();
+	            System.out.println(a + b);
+	        }
+	}
+
+}
